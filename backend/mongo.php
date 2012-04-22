53cf0ce785815f59a7f1e204d24d15037060faa6
Bilal Quadri <bilalquadri92@gmail.com>
2012-04-22 02:30:45 -0400

    Renamed things

--- backend/mongo.php
+++ backend/mongo.php
@@ -1,7 +1,7 @@
 <?php
 try {
   $m = new Mongo();
-  $db = $m->selectDB('trend');
+  $db = $m->selectDB('trends');
   $collection = $db->selectCollection('users');  
 }
 catch ( MongoConnectionException $e ) {
--- backend/trendsetter.php
+++ backend/trendsetter.php
@@ -1,3 +1,5 @@
 <?php
   require_once('mongo.php');
+
+  $query = "https://api.twitter.com/1/statuses/home_timeline.json?include_entities=true?count=200";
   
